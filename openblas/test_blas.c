e4567d75101ef304c61f7d377abf055802795324
Milan Skocic <milan.skocic@icloud.com>
2022-10-22 10:09:17 +0200

    ENH: Add doc in example for test_blas.

--- openblas/test_blas.c
+++ openblas/test_blas.c
@@ -108,9 +108,6 @@ int main()
     char TRANSA = 'N';
     char TRANSB = 'N';
     double ALPHA = 0.1;
-    int LDA = k;
-    int LDB = n;
-    int LDC = n;
     double BETA = 2.21;
 
     // read row major memory with column major algorithm
@@ -131,11 +128,20 @@ int main()
     // Reading in col major convention gives C* (3*2)
     // common stride is 3 (number of cols in row major and number of rows in col major).
     // Normal call to dgemm with fortran would be
-    // dgemm_(&TRANSA, &TRANSB,
-    //                &M, &N, &K,
-    //                &ALPHA, A, &LDA,
-    //                B, &LDB, &BETA, C, &LDC);
-    // A and B are inversed in the call as we are dealing with the transposes
+    // LDA = m if TRANSA = N and LDA = k if TRANSA = T
+    // LDB = k if TRANSB = N and LDB = n if TRANSB = T
+    // LDC = m if TRANSC = N and LDC = n if TRANSC = T
+    // dgemm(TRANSA, TRANSB,
+    //                M, N, K,
+    //                ALPHA, A, LDA,
+    //                B, LDB, BETA, C, LDC);
+
+    // A and B are inversed in the call as we are dealing with the transposes 
+    // du to the row major memory layout which corresponds
+    // therefore LDA, LDB, LDC corresponds to the value when TRANSA, TRANSB and TRANSC are equal to T
+    int LDA = k;
+    int LDB = n;
+    int LDC = n;
     dgemm_(&TRANSA, &TRANSB,
                     &n, &m, &k,
                     &ALPHA, B, &LDB,
@@ -157,6 +163,7 @@ int main()
      LDC = n;
 
     // CBLAS takes care of row major column memeory layout in C vs column major memory layout in Fortran.
+    // CBLAS will inverse A and B but LDA, LDB and LDC must be set to the values when TRANSX = T
     // When it is possible it is easier to use CBLAS from C program.
      cblas_dgemm(CblasRowMajor, CblasNoTrans,
                  CblasNoTrans, m, n,
