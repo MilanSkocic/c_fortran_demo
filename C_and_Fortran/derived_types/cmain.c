27caee69b3a0000443ddf39bdcb3c8606f4f2df9
Milan Skocic <milan.skocic@icloud.com>
2023-12-14 11:14:38 +0100

    ENH: Add complex derived type for C operability.

--- C_and_Fortran/derived_types/cmain.c
+++ C_and_Fortran/derived_types/cmain.c
@@ -8,15 +8,19 @@ int main(void){
     int i;
 
     /*SIMPLE FORTRAN DERIVED TYPE*/
+    printf("%s\n", obj_simple_capi.name);
     printf("%d\n", obj_simple_capi.n);
     for(i=0;i<3;i++){
         printf("%f\t", obj_simple_capi.array[i]);
     }
-    printf("%s\n", obj_simple_capi.name);
+    printf("\n");
 
-    void *obj = fobj_cmpl_capi_init(3, 4.5);
-    fobj_cmpl_capi_print(obj);
-    fobj_cmpl_capi_del(obj);
+    struct objcmpl *obj = objcmpl_init(3, 6.0);
+    obj->print(obj);
+    char *name = objcmpl_capi_getname(obj->fobj);
+    printf("%s\n", name);
+    objcmpl_del(obj);
+
 
     return EXIT_SUCCESS;
 }
--- C_and_Fortran/derived_types/fobject_cmpl.f90
+++ C_and_Fortran/derived_types/fobject_cmpl.f90
@@ -8,69 +8,85 @@ module fobject_cmpl
     ! The trick is to pass the derived type to C as pointer (void *)
     ! wrap all bound procedures with procedures that will accept a C pointer void*
 
-    type :: fobj_cmpl
+    type :: objcmpl
         integer(int32) :: n
         real(real32), pointer :: array(:)
+        character(len=:), pointer :: name
     contains
-        procedure :: print => fobj_cmpl_print
+        procedure :: print => objcmpl_print
     end type
 
 contains
 
-subroutine fobj_cmpl_print(self)
+subroutine objcmpl_print(self)
     implicit none
-    class(fobj_cmpl), intent(in) :: self
+    class(objcmpl), intent(in) :: self
+    print *, self%name
     print *, self%n
     print *, self%array
 end subroutine
 
-subroutine fobj_cmpl_del(o)
+subroutine objcmpl_del(o)
     implicit none
-    type(fobj_cmpl), intent(inout), pointer :: o
+    type(objcmpl), intent(inout), pointer :: o
     deallocate(o%array)
+    deallocate(o%name)
     deallocate(o)
 end subroutine
 
-pure function fobj_cmpl_init(n, value)result(p)
+pure function objcmpl_init(n, value)result(p)
     implicit none
     integer(int32), intent(in) :: n
     real(real32), intent(in) :: value
-    type(fobj_cmpl), pointer :: p
+    type(objcmpl), pointer :: p
     
     allocate(p)
     allocate(p%array(n))
+    allocate(p%name, source="F OBJECT"//c_null_char)
     
     p%n = n
     p%array = value
 end function
 
-subroutine fobj_cmpl_capi_print(o)bind(c)
+function objcmpl_capi_getname(o)result(name)bind(c)
     implicit none
     type(c_ptr), intent(in), value :: o
-    type(fobj_cmpl), pointer :: fp
+    type(c_ptr) :: name
+    type(objcmpl), pointer :: fp
+    nullify(fp)
+    call c_f_pointer(o, fp)
+
+    name = c_loc(fp%name)
+
+end function
+
+subroutine objcmpl_capi_print(o)bind(c)
+    implicit none
+    type(c_ptr), intent(in), value :: o
+    type(objcmpl), pointer :: fp
     nullify(fp)
     call c_f_pointer(o, fp)
     call fp%print()
 end subroutine
 
-subroutine fobj_cmpl_capi_del(o)bind(c)
+subroutine objcmpl_capi_del(o)bind(c)
     implicit none
     type(c_ptr), intent(in), value :: o
-    type(fobj_cmpl), pointer :: fp
+    type(objcmpl), pointer :: fp
     nullify(fp)
     call c_f_pointer(o, fp)
-    call fobj_cmpl_del(fp)
+    call objcmpl_del(fp)
 end subroutine
 
-pure function fobj_cmpl_capi_init(n, value)result(p)bind(c)
+pure function objcmpl_capi_init(n, value)result(p)bind(c)
     implicit none
     integer(c_int), intent(in), value :: n
     real(c_float), intent(in), value :: value
 
     type(c_ptr) :: p
 
-    type(fobj_cmpl), pointer :: fp
-    fp => fobj_cmpl_init(n, value)
+    type(objcmpl), pointer :: fp
+    fp => objcmpl_init(n, value)
     p = c_loc(fp)
 end function
 
