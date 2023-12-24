766948dbbd315d889fc3721d6d3454d077292fa1
Milan Skocic <milan.skocic@icloud.com>
2023-12-24 21:15:43 +0100

    BLD: Add support for mingw32 on windows.

--- Python/f2py/setup.py
+++ Python/f2py/setup.py
@@ -1,6 +1,20 @@
+import platform
 from setuptools import find_packages
 import numpy
-from numpy.distutils.core import Extension, setup
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
+
+
+class CustomBuildExt( build_ext ):
+    r"""Custom class for building extension."""
+    def build_extensions(self):
+        r"""Custom building function for mingw32 on windows"""
+        c = self.compiler.compiler_type
+        if (c == "mingw32") and (platform.system() == "Windows"):
+           for e in self.extensions:
+               e.extra_link_args += ["-static"]
+        build_ext.build_extensions(self)
+
 
 fib_c_ext = Extension(name="example.fib_c",
                       sources=["./example/_fib.c", "./example/fib.c"],
@@ -17,5 +31,6 @@ setup(
     version="1.0",
     packages=find_packages(),
     include_package_data=True,
-    ext_modules=[fib_fortran_ext, fib_c_ext]
+    cmdclass = {'build_ext': CustomBuildExt},
+    ext_modules=[fib_c_ext]
 )
